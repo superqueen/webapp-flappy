256894b7e8620d278822a444809881dde1841319
superqueen <jennifer.theou@gmail.com>
2015-07-25 22:52:35 +0100

    Day 1 of tutorial Chap 3 LA 4

--- js/flappy.js
+++ js/flappy.js
@@ -13,6 +13,9 @@ var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
  * Loads all resources for the game and gives them names.
  */
 function preload() {
+    game.load.image("playerImg", "../assets/jamesBond.gif");
+    game.load.audio("score", "../assets/point.ogg");
+
 
 
 }
@@ -22,11 +25,31 @@ function preload() {
  */
 function create() {
     // set the background colour of the scene
+    game.stage.setBackgroundColor("66FFFF");
+    game.add.text(20, 20, "Welcome to my Game!", {
+        font: "30px Arial",
+        fill: "#FFFFFF",
+        stroke: "000000",
+        strokeThickness: 10
+    });
+    game.add.sprite(395, 200, "playerImg");
+
+    game.input.onDown.add(clickHandler);
+    game.input
+        .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.add(spaceHandler);
 }
 
+
+function clickHandler(){
+    game.add.sprite(event.x, event.y, "playerImg");
+}
+
+function spaceHandler() {
+    game.sound.play("score");
+}
 /*
  * This function updates the scene. It is called for every new frame.
  */
 function update() {
-
 }
